f6edbe9fb684d8d276ed43d972b45dc3d2ea88a1
zfish617 <zjf219@seas.upenn.edu>
2020-11-12 19:31:28 -0500

    spatial stream

--- TwoStream.py
+++ TwoStream.py
@@ -5,35 +5,77 @@ from torch import nn, Tensor
 from dataset import *
 import random
 import torchvision
+import torchvision.models as models
+import torch.optim as optim
 
 
-class TwoStream(torch.nn.Module):
 
-    def __init__(self, device='cuda'):
-        # Initialize the two-stream layers
-        super(TwoStream, self).__init__()
+class SpatialStream(torch.nn.Module):
 
+    def __init__(self, 
+                 device='cuda',
+                 num_classes=51,
+                 dropout_probability=0.5):
+
+        # Initialize the stream layers
+        super(SpatialStream, self).__init__()
         self.device = device
+        self.num_classes = num_classes
 
-        # Define Spatial Layer
+        # Spatial Backbone
+        self.spatial = models.resnet50(pretrained=True)
+        for param in self.spatial.parameters():
+            param.requires_grad = False  # False: Freezes the weights of the pre-trained model
+
+        # Add to Spatial Backbone
+        self.spatial.fc = nn.Sequential(nn.Linear(2048, 1024),
+                                nn.ReLU(),
+                                nn.Dropout(p=dropout_probability),
+                                nn.Linear(1024, self.num_classes),
+                                nn.ReLU(),
+                                nn.Dropout(p=dropout_probability),
+                                nn.Softmax())
+
+    def forward(self, X):
+        return self.spatial(X)
+
+    def compute_loss(self, clas_out, regr_out, targ_clas, targ_regr, l=1, effective_batch=50):
+        pass
+
+    def postprocess(self, out_c, out_r, IOU_thresh=0.5, keep_num_preNMS=50, keep_num_postNMS=10):
+        pass
 
 
-        # Define Temporal Layers
+class TemporalStream(torch.nn.Module):
+
+    def __init__(self, 
+                 device='cuda',
+                 num_classes=51,
+                 dropout_probability=0.5):
+
+        # Initialize the stream layers
+        super(TemporalStream, self).__init__()
+        self.device = device
+        self.num_classes = num_classes
 
 
 
     def forward(self, X):
         pass
 
-
     def compute_loss(self, clas_out, regr_out, targ_clas, targ_regr, l=1, effective_batch=50):
         pass
-
 
     def postprocess(self, out_c, out_r, IOU_thresh=0.5, keep_num_preNMS=50, keep_num_postNMS=10):
         pass
 
 
-
 if __name__ == "__main__":
-    pass
+
+    # Spatial
+    spatial_stream = SpatialStream()
+    spatial_optimizer = optim.SGD(spatial_stream.parameters(), lr=0.1, momentum=0.9)
+
+    # Temporal
+    
+
